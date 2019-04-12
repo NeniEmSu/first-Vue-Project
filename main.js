d77526d9af1ac391baae012e119efef43ee356aa
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-04-12 14:09:15 +0300

    practiced use of v-if, v-for, creating methods and data for vue

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
+var navigation = new Vue({
+    el: '#navigation',
+    data: {
+        home: "./index.html",
+        about: "./about.html"
+    }
+})
+
 var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'These socks are the the best socks you will ever wear'
+        image: "./img/vmSocks-green-onWhite.jpg",
+        description: 'These socks are the the best socks you will ever wear',
+        inStock: true,
+        inventory: 2,
+        onSale: true,
+        items: [
+            "80% cotton",
+            "20% polyester",
+            "Gender-neutral"
+        ],
+        variants: [{
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+
+        ],
+        sizes: [
+            "ExtraLarge",
+            "large",
+            "Medium",
+            "Small",
+            "Tiny"
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        }
     }
 })
