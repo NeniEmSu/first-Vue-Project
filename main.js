4f096d077984bed989afd07404f05afdcd223bde
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-04-12 23:43:38 +0300

    creating components

--- main.js
+++ main.js
@@ -1,3 +1,124 @@
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+     <div class="product">
+          
+        <div class="product-image">
+          <img :src="image" />
+        </div>
+  
+        <div class="product-info">
+        <div class="cart">
+              <p>Cart({{ cart }})</p>
+            </div>
+            <h1>{{ product }}</h1>
+            <p>{{ description }}</p>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>{{ sale }}</p>
+            <p>Shipping: {{ shipping }}</p>
+  
+            <product-details :details="details"></product-details>
+  
+            <div class="color-box"
+                 v-for="(variant, index) in variants" 
+                 :key="variant.variantId"
+                 :style="{ backgroundColor: variant.variantColor }"
+                 @mouseover="updateProduct(index)"
+                 >
+            </div> 
+
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+                <ol>
+                    <li v-for="size in sizes">{{ size }}</li>
+                </ol>
+  
+            <div><button v-on:click="addToCart" :disabled="!inStock" :class="{disabledButton: !inStock}">Add to
+                        Cart</button>
+                </div>
+                <div><button v-on:click="decrimentFromCart" v-show="cart > 0">Dectrment From cart</button></div>
+  
+         </div>  
+      
+      </div>
+     `,
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            description: "The socks thats gives you mastery over the most Beginer friendly Js framework ever!",
+            selectedVariant: 0,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [{
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            sizes: [
+                "ExtraLarge",
+                "large",
+                "Medium",
+                "Small",
+                "Tiny"
+            ],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        },
+        decrimentFromCart: function () {
+            this.cart -= 1
+        },
+        updateProduct: function (index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale &&
+                this.variants[this.selectedVariant].variantQuantity < 5 && this.variants[this.selectedVariant].variantQuantity != 0) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            } else if (this.variants[this.selectedVariant].variantQuantity == 0) {
+                return this.brand + ' ' + this.product + ' are no longer in Stock.'
+            }
+
+            return this.brand + ' ' + this.product + ' are not on sale'
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return 2.99
+        }
+    }
+})
+
 var navigation = new Vue({
     el: '#navigation',
     data: {
@@ -9,69 +130,6 @@ var navigation = new Vue({
 var app = new Vue({
     el: '#app',
     data: {
-        brand: "Vue Mastery",
-        product: 'Socks',
-        selectedVariant: 0,
-        description: 'These socks are the the best socks you will ever wear',
-        onSale: true,
-        items: [
-            "80% cotton",
-            "20% polyester",
-            "Gender-neutral"
-        ],
-        variants: [{
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: "./img/vmSocks-green-onWhite.jpg",
-                variantQuantity: 0
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: "./img/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 10
-            }
-
-        ],
-        sizes: [
-            "ExtraLarge",
-            "large",
-            "Medium",
-            "Small",
-            "Tiny"
-        ],
-        cart: 0
-    },
-    methods: {
-        // using normal javascript es5 function
-        addToCart: function () {
-            this.cart += 1
-        },
-        // using es6 functiion decletation
-
-        decrimentFromCart: function () {
-            this.cart -= 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index
-        }
-
-    },
-    computed: {
-        title() {
-            return this.brand + " " + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' are on sale!'
-            }
-            return this.brand + ' ' + this.product + ' are not on sale'
-        }
+        premium: true
     }
 })
