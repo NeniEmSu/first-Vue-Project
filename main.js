64b33f22edf9a4921aa8740352e57c75e265fda8
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-04-12 22:34:04 +0300

    practiced using computed properties to output dynamic data

--- main.js
+++ main.js
@@ -9,11 +9,10 @@ var navigation = new Vue({
 var app = new Vue({
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: 'Socks',
-        image: "./img/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         description: 'These socks are the the best socks you will ever wear',
-        inStock: true,
-        inventory: 2,
         onSale: true,
         items: [
             "80% cotton",
@@ -23,12 +22,14 @@ var app = new Vue({
         variants: [{
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: "./img/vmSocks-green-onWhite.jpg"
+                variantImage: "./img/vmSocks-green-onWhite.jpg",
+                variantQuantity: 0
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: "./img/vmSocks-blue-onWhite.jpg"
+                variantImage: "./img/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 10
             }
 
         ],
@@ -51,9 +52,26 @@ var app = new Vue({
         decrimentFromCart: function () {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
         }
 
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return this.brand + ' ' + this.product + ' are not on sale'
+        }
     }
 })
